3514516964b915a625ab5388c49734a4fd6bc3f2
Lynn <43623996+LinmeiJ@users.noreply.github.com>
2024-11-12 18:09:16 -0500

    Update env.js

--- cw-synthetic-canaries/src/utils/env.js
+++ cw-synthetic-canaries/src/utils/env.js
@@ -1,17 +1,1 @@
-exports.getEnvPath = async () => {
-  // Set url path per environment
-  let envPath = '';
-  switch (process.env.NODE_ENV) {
-    case 'dev':
-      envPath = 'ngp.dev.geaerospace.net';
-      break;
-    case 'qa':
-      envPath = 'ngp.qa.geaerospace.com';
-      break;
-    case 'prod':
-      envPath = 'ngp.geaerospace.com';
-      break;
-  }
 
-  return envPath;
-}
