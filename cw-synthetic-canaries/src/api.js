a1bafb20ce66f69843e17f4303c807edbc19ef2e
Mills, Linmei <43623996+LinmeiJ@users.noreply.github.com>
2024-11-13 09:27:41 -0500

    fix merge conflict

--- cw-synthetic-canaries/src/api.js
+++ cw-synthetic-canaries/src/api.js
@@ -1,45 +1,45 @@
-//const synthetics = require('Synthetics');
-//const oidc = require('utils/oidc');
-//const log = require('SyntheticsLogger');
-//
-//const apiCallback = function (response) {
-//  return new Promise((resolve, reject) => {
-//    log.info('statusCode:', response.statusCode);
-//    if (response.statusCode !== 200) {
-//      throw `API call did not return a 200. It's statusCode was "${response.statusCode}" with "${response.statusMessage}" as the statusMessage.`;
-//    }
-//
-//    let responseBody = '';
-//    response.on('data', (data) => {
-//      responseBody += data;
-//    });
-//
-//    response.on('end', () => {
-//      log.info('responseBody:', responseBody);
-//      resolve('Successfully completed api check');
-//    });
-//  });
-//}
-//
-//exports.handler = async (ENV_PATH, DEFAULT_STEP_CONFIG) => {
-//
-//  const token = await oidc.handler();
-//
-//  const api = {
-//    hostname: `ngp-api${ENV_PATH}.av.ge.com`,
-//    method: 'GET',
-//    path: '/api/v2/site',
-//    port: 443,
-//    protocol: 'https:',
-//    headers: {
-//      Authorization : `Bearer ${token}`,
-//      'Content-Type': 'application/json',
-//    },
-//    rejectUnauthorized: false,
-//  }
-//
-//  await synthetics.executeHttpStep('Test oidc token with api endpoint', api, apiCallback, DEFAULT_STEP_CONFIG);
-//
-//  return 'API checks finished'
-//
-//}
+const synthetics = require('Synthetics');
+const oidc = require('utils/oidc');
+const log = require('SyntheticsLogger');
+
+const apiCallback = function (response) {
+  return new Promise((resolve, reject) => {
+    log.info('statusCode:', response.statusCode);
+    if (response.statusCode !== 200) {
+      throw `API call did not return a 200. It's statusCode was "${response.statusCode}" with "${response.statusMessage}" as the statusMessage.`;
+    }
+
+    let responseBody = '';
+    response.on('data', (data) => {
+      responseBody += data;
+    });
+
+    response.on('end', () => {
+      log.info('responseBody:', responseBody);
+      resolve('Successfully completed api check');
+    });
+  });
+}
+
+exports.handler = async (ENV_PATH, DEFAULT_STEP_CONFIG) => {
+
+  const token = await oidc.handler();
+
+  const api = {
+    hostname: `ngp-api${ENV_PATH}.av.ge.com`,
+    method: 'GET',
+    path: '/api/v2/site',
+    port: 443,
+    protocol: 'https:',
+    headers: {
+      Authorization : `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+    rejectUnauthorized: false,
+  }
+
+  await synthetics.executeHttpStep('Test oidc token with api endpoint', api, apiCallback, DEFAULT_STEP_CONFIG);
+
+  return 'API checks finished'
+
+}
--- cw-synthetic-canaries/src/main.js
+++ cw-synthetic-canaries/src/main.js
@@ -1,1 +1,26 @@
-
+//const { getEnvPath } = require('utils/env');
+//const log = require('SyntheticsLogger');
+//const login = require('login');
+//const healthcheck = require('heartbeat');
+////const api = require('api');
+//
+//exports.handler = async () => {
+//  const URL = await getURL();
+//  const DEFAULT_STEP_CONFIG = {
+//    'continueOnStepFailure': false,
+//    'screenshotOnStepStart': false,
+//    'screenshotOnStepSuccess': true,
+//    'screenshotOnStepFailure': true
+//  };
+//
+//  await heartbeat.handler(URL, DEFAULT_STEP_CONFIG);
+//  log.debug('healthcheck passed');
+//
+//  await login.handler(URL, DEFAULT_STEP_CONFIG);
+//  log.debug('login passed');
+//
+//  await api.handler(URL, DEFAULT_STEP_CONFIG);
+//  log.debug('api passed');
+//
+//  return 'All checks completed';
+//}
